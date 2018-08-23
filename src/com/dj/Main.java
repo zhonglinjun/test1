921132bbcfdb7843d5aa00f3107736405bbe477b
zhonglinjun <zhonglinjun@users.noreply.github.com>
2018-08-23 20:27:50 +0800

    Update Main.java

--- src/com/dj/Main.java
+++ src/com/dj/Main.java
@@ -31,7 +31,7 @@ public class Main {
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
 				System.out.println(rs.getString("name"));
-				System.out.println(44445555555);
+				System.out.println(33333333);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
